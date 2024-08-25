c1108422175f67ed7f3c96c53975156f49194f59
Lin Htut Kyaw <linhtutkyaw.dev@gmail.com>
2024-08-25 13:08:59 +0630

    ok

--- app/(root)/(home)/team.tsx
+++ app/(root)/(home)/team.tsx
@@ -3,12 +3,10 @@ const Team = () => {
     <section className="py-6 dark:bg-gray-100 dark:text-gray-800">
       <div className="container flex flex-col items-center justify-center p-4 mx-auto space-y-8 sm:p-10">
         <h1 className="text-4xl font-bold leading-none text-center sm:text-5xl">
-          Our team
+          Our Passionate Team
         </h1>
         <p className="max-w-2xl text-center dark:text-gray-600">
-          At a assumenda quas cum earum ut itaque commodi saepe rem aspernatur
-          quam natus quis nihil quod, hic explicabo doloribus magnam neque,
-          exercitationem eius sunt!
+          United by a Shared Vision for Success!
         </p>
         <div className="flex flex-row flex-wrap-reverse justify-center">
           <div className="flex flex-col justify-center m-8 text-center">
